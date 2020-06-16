3a8117bf4cb998b3d57722627794593c41d1daff
Angel Guevara, Jr <angel.guevara@valiant.com>
2020-06-15 21:06:43 -0400

    Split up the Replacer class

---
+++ src/FindAndReplace/Replacer.ReplaceResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace FindAndReplace
+{
+
+    public partial class Replacer
+	{
+        public class ReplaceResult
+		{
+			public List<ReplaceResultItem> ResultItems { get; set; }
+
+			public Stats Stats { get; set; }
+		}
+	}
+}
---
+++ src/FindAndReplace/ReplacerEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FindAndReplace
+{
+    public class ReplacerEventArgs : EventArgs
+	{
+		public Replacer.ReplaceResultItem ResultItem { get; set; }
+		public Stats Stats { get; set; }
+		public Status Status { get; set; }
+		public bool IsSilent { get; set; }
+
+		public ReplacerEventArgs(Replacer.ReplaceResultItem resultItem, Stats stats, Status status, bool isSilent = false)
+		{
+			ResultItem = resultItem;
+			Stats = stats;
+			Status = status;
+			IsSilent = isSilent;
+		}
+	}
+}

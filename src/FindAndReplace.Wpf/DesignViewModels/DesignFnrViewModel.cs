288ae81808ea14f797d579c062820859595641b4
Angel Guevara, Jr <angel.guevara@valiant.com>
2020-06-30 15:37:57 -0400

    Added design-time encodings

--- src/FindAndReplace.Wpf/DesignViewModels/DesignFnrViewModel.cs
+++ src/FindAndReplace.Wpf/DesignViewModels/DesignFnrViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using FindAndReplace.Wpf.ModelFactories;
 using FindAndReplace.Wpf.Models;
 using FindAndReplace.Wpf.ViewModels;
@@ -10,6 +12,7 @@ namespace FindAndReplace.Wpf.DesignViewModels
         public DesignFnrViewModel()
             : base(null, null, null, null, null, null, null)
         {
+            Encodings = new List<String> { "Encoding 1", "Encoding 2", "Encoding 3" };
             FolderParameters = FolderParametersFactory.GetFolderParameters(100);
             FindParameters = FindParametersFactory.GetFindParameters(100);
             ReplaceParameters = ReplaceParametersFactory.GetReplaceParameters(100);

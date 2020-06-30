ddc44be3fc84e337a987620f5687e5acc94f41b5
Angel Guevara, Jr <angel.guevara@valiant.com>
2020-06-30 17:15:27 -0400

    Refactored the FolderParameters properties to use collections instead of comma-separated strings

--- src/FindAndReplace.Wpf/ModelFactories/FolderParametersFactory.cs
+++ src/FindAndReplace.Wpf/ModelFactories/FolderParametersFactory.cs
@@ -1,17 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using FindAndReplace.Wpf.Models;
 
 namespace FindAndReplace.Wpf.ModelFactories
 {
     public static class FolderParametersFactory
     {
+        private static IEnumerable<String> GetDirectories(int count)
+        {
+            for (var index = 0; index < count; index++)
+                yield return @$"C:\Example\Path\To\Folder{index + 1}";
+        }
+
+        private static IEnumerable<String> GetFileMasks(int count)
+        {
+            for (var index = 0; index < count; index++)
+                yield return $"*.ext{index}";
+        }
+
         public static FolderParameters GetFolderParameters(int seed)
         {
+            var directories = GetDirectories(10);
+            var fileExtensions = GetFileMasks(10);
+
             var fp = new FolderParameters
             {
-                ExcludeDirectories = $"Exclude Directories {seed}",
-                ExcludeMask = $"Exclude Mask {seed}",
-                FileMask = $"File Mask {seed}",
+                ExcludeDirectories = new ObservableCollection<string>(directories),
+                ExcludeFiles = new ObservableCollection<string>(fileExtensions),
+                IncludeFiles = new ObservableCollection<string>(fileExtensions),
                 IsRecursive = seed % 2 == 0,
                 RootDirectory = $"Root Directory {seed}"
             };

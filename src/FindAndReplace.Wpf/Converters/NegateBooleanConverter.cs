f017bba88ae9d239e4a82057e9d83946f8ae20c7
Angel Guevara, Jr <angel.guevara@valiant.com>
2020-08-30 01:00:40 -0400

    Implement File Discovery Process
    Added the "Search Filename Only" checkbox

---
+++ src/FindAndReplace.Wpf/Converters/NegateBooleanConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace FindAndReplace.Wpf.Converters
+{
+    public class NegateBooleanConverter : MarkupExtension, IValueConverter
+    {
+        private static NegateBooleanConverter _converter = null;
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (_converter == null)
+                _converter = new NegateBooleanConverter();
+            return _converter;
+        }
+
+        private bool Convert(object value)
+        {
+            if (value == null || !(value is bool))
+                return false;
+
+            var boolValue = System.Convert.ToBoolean(value);
+            return !boolValue;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value);
+        }
+
+    }
+}

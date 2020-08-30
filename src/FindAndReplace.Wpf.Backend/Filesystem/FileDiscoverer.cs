f7f4a699d4aff55c631434970a05da921573b0a7
Angel Guevara, Jr <angel.guevara@valiant.com>
2020-08-30 01:15:20 -0400

    Implement File Discovery Process
    Added the new "FileDiscoverer" srevice

---
+++ src/FindAndReplace.Wpf.Backend/Filesystem/FileDiscoverer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindAndReplace.Wpf.Backend.Filesystem
+{
+    public interface IFileDiscoverer
+    {
+
+    }
+
+    public class FileDiscoverer : IFileDiscoverer
+    {
+
+    }
+}

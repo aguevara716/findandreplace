25da6f9afd5bfbd15236a7abe41b9cf21369bf95
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-09-02 00:57:54 -0400

    Implement File Discovery Process
    Implemented the IFileDiscoverer

---
+++ src/FindAndReplace.Wpf.Backend.Tests/Filesystem/FileDiscovererTests.cs
@@ -0,0 +1,47 @@
+﻿using FindAndReplace.Wpf.Backend.Filesystem;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace FindAndReplace.Wpf.Backend.Tests.Filesystem
+{
+    [TestFixture]
+    public class FileDiscovererTests
+    {
+        private IFileDiscoverer _fileDiscoverer;
+        private IFileFilterer _fileFilterer;
+        private IFileRetriever _fileRetriever;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _fileFilterer = Substitute.For<IFileFilterer>();
+            _fileRetriever = Substitute.For<IFileRetriever>();
+
+            _fileDiscoverer = new FileDiscoverer(_fileFilterer, _fileRetriever);
+        }
+
+        // FileDiscoveryResult DiscoverFiles(string rootDirectory, 
+        //    IList<string> fileMasks, 
+        //    IList<string> excludedDirectories, 
+        //    IList<string> excludedFileMasks, 
+        //    bool isRecursive)
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void DiscoverFiles_Should_ReturnFailureIfRootDirectoryIsNullOrEmpty(bool isNull)
+        {
+            string rootDirectory = isNull
+                ? null
+                : string.Empty;
+
+            var fileDiscoveryResult = _fileDiscoverer.DiscoverFiles(rootDirectory, null, null, null, false);
+
+            fileDiscoveryResult.IsSuccessful.Should().BeFalse();
+            _fileRetriever.DidNotReceiveWithAnyArgs().GetFiles(null, null, false);
+            _fileFilterer.DidNotReceiveWithAnyArgs().FilterOutExcludedDirectories(null, null, null, false);
+            _fileFilterer.DidNotReceiveWithAnyArgs().FilterOutExcludedFileMasks(null, null, null);
+        }
+
+    }
+}
--- src/FindAndReplace.Wpf.Backend/Filesystem/FileDiscoverer.cs
+++ src/FindAndReplace.Wpf.Backend/Filesystem/FileDiscoverer.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using FindAndReplace.Wpf.Backend.Results;
 
 namespace FindAndReplace.Wpf.Backend.Filesystem
 {
     public interface IFileDiscoverer
     {
-
+        FileDiscoveryResult DiscoverFiles(string rootDirectory,
+                                          IList<string> fileMasks,
+                                          IList<string> excludedDirectories,
+                                          IList<string> excludedFileMasks,
+                                          bool isRecursive);
     }
 
     public class FileDiscoverer : IFileDiscoverer
     {
+        private readonly IFileFilterer _fileFilterer;
+        private readonly IFileRetriever _fileRetriever;
 
+        public FileDiscoverer(IFileFilterer fileFilterer, IFileRetriever fileRetriever)
+        {
+            _fileFilterer = fileFilterer;
+            _fileRetriever = fileRetriever;
+        }
+
+        public FileDiscoveryResult DiscoverFiles(string rootDirectory, 
+                                                 IList<string> fileMasks, 
+                                                 IList<string> excludedDirectories, 
+                                                 IList<string> excludedFileMasks,
+                                                 bool isRecursive)
+        {
+            if (String.IsNullOrEmpty(rootDirectory))
+                return FileDiscoveryResult.CreateFailure<FileDiscoveryResult>(rootDirectory, "Root directory is required");
+
+            var filesMatchingMasksResult = _fileRetriever.GetFiles(rootDirectory, fileMasks, isRecursive);
+            if (!filesMatchingMasksResult.IsSuccessful)
+                return filesMatchingMasksResult;
+
+            var filesInDirectory = filesMatchingMasksResult.Files;
+
+            var filesFilteredByExcludedDirectoriesResult = _fileFilterer.FilterOutExcludedDirectories(rootDirectory, filesInDirectory, excludedDirectories, isRecursive);
+            if (!filesFilteredByExcludedDirectoriesResult.IsSuccessful)
+                return filesFilteredByExcludedDirectoriesResult;
+
+            var filesFilteredByExcludedFileMasksResult = _fileFilterer.FilterOutExcludedFileMasks(rootDirectory, filesInDirectory, excludedFileMasks);
+            return filesFilteredByExcludedFileMasksResult;
+        }
     }
 }

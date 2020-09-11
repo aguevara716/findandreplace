a3accd71bbae2fcc918099e83e688d6671b6e459
Angel Guevara, Jr <angel.guevara@valiant.com>
2020-09-11 00:19:07 -0400

    Implement Find Process
    Added more properties to the FileResult model

--- src/FindAndReplace.Wpf/Models/FileResult.cs
+++ src/FindAndReplace.Wpf/Models/FileResult.cs
@@ -1,4 +1,7 @@
-﻿namespace FindAndReplace.Wpf.Models
+﻿using System.Collections.Generic;
+using FindAndReplace.Wpf.Backend.Results;
+
+namespace FindAndReplace.Wpf.Models
 {
     public class FileResult
     {
@@ -6,5 +9,8 @@
         public string Filename { get; set; }
         public string RelativePath { get; set; }
         public string Extension { get; set; }
+        public List<TextMatch> TextMatches { get; set; }
+        public bool HasError { get; set; }
+        public string ErrorMessage { get; set; }
     }
 }

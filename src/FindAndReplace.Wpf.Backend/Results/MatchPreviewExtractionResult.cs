2cb44f3a1744576cf762401d589f8fb8f7a361c3
Angel Guevara, Jr <angel.guevara@valiant.com>
2020-09-11 18:15:31 -0400

    Display Match Previews
    Added preview models

---
+++ src/FindAndReplace.Wpf.Backend/Results/MatchPreviewExtractionResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace FindAndReplace.Wpf.Backend.Results
+{
+    public class MatchPreviewExtractionResult : BaseResult<List<PreviewText>>
+    {
+        public List<PreviewText> PreviewText { get { return Payload; } }
+    }
+
+}
---
+++ src/FindAndReplace.Wpf.Backend/Results/PreviewText.cs
@@ -0,0 +1,10 @@
+﻿namespace FindAndReplace.Wpf.Backend.Results
+{
+    public class PreviewText
+    {
+        public int MatchStartingLineNumber { get; set; }
+        public int MatchEndingLineNumber { get; set; }
+        public string Content { get; set; }
+    }
+
+}

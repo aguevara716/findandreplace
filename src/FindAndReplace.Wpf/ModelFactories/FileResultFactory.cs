81e0c5aa6634c8ea195858478e201ce1221e5876
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-09-13 14:37:10 -0400

    Display Match Previews
    Changed preview text so that it spans more than 1 line

--- src/FindAndReplace.Wpf/ModelFactories/FileResultFactory.cs
+++ src/FindAndReplace.Wpf/ModelFactories/FileResultFactory.cs
@@ -14,7 +14,7 @@ namespace FindAndReplace.Wpf.ModelFactories
         private static IEnumerable<string> GetPreviews(int count, int seed)
         {
             for (var index = 0; index < count; index++)
-                yield return $"Preview text #{index + 1} for item {seed}";
+                yield return $"({index + 1} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Quam id leo in vitae turpis massa sed elementum tempus. Integer feugiat scelerisque varius morbi. Malesuada fames ac turpis egestas maecenas pharetra convallis posuere morbi.\n\nAccumsan tortor posuere ac ut consequat semper viverra nam libero. Tortor consequat id porta nibh venenatis cras. Ac turpis egestas maecenas pharetra convallis posuere. Augue eget arcu dictum varius duis at. Diam in arcu cursus euismod quis viverra nibh. Sed turpis tincidunt id aliquet risus feugiat in ante. Et netus et malesuada fames ac turpis egestas.\n\nScelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Sit amet nisl purus in mollis. Feugiat pretium nibh ipsum consequat.\n\nPurus sit amet volutpat consequat mauris. Orci phasellus egestas tellus rutrum.";
         }
 
         public static FileResult GetFileResult(int seed)

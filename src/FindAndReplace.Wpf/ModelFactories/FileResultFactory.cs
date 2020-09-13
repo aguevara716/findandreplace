ed48bba5f3f9cd821a85067ecd6264a80b9178c5
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-09-13 12:33:30 -0400

    Display Match Previews
    Misc changes

--- src/FindAndReplace.Wpf/ModelFactories/FileResultFactory.cs
+++ src/FindAndReplace.Wpf/ModelFactories/FileResultFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using FindAndReplace.Wpf.Models;
 
 namespace FindAndReplace.Wpf.ModelFactories
@@ -10,16 +11,25 @@ namespace FindAndReplace.Wpf.ModelFactories
             ".txt", ".cs", ".pdf", ".docx"
         };
 
+        private static IEnumerable<string> GetPreviews(int count, int seed)
+        {
+            for (var index = 0; index < count; index++)
+                yield return $"Preview text #{index + 1} for item {seed}";
+        }
+
         public static FileResult GetFileResult(int seed)
         {
             var extension = EXTENSIONS[seed % EXTENSIONS.Length];
             var filename = $"File-{seed}{extension}";
             var fr = new FileResult
             {
+                ErrorMessage = $"Error message {seed}",
                 Extension = extension,
                 Filename = filename,
                 FullPath = $@"C:\Root\Directory\Subdirectory\{filename}",
-                RelativePath = @"Directory\Subdirectory"
+                HasError = seed % 2 == 0,
+                Previews = GetPreviews(5, seed).ToList(),
+                RelativePath = @"Directory\Subdirectory",
             };
             return fr;
         }
